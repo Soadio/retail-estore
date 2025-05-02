9354ca30362b8e508c0c511345581a60c27447ca
Sheriff Adio <flamezee15@gmail.com>
2025-05-02 15:20:14 +0100

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 export default function Home() {
   return (
-    <div className="text-rose-600">
-      <p>
-        Hello world
-      </p> 
+      <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant={"elevated"}>
+          Button
+        </Button>
+      </div>
+
+      <div>
+        <Input placeholder=" Input"/>
+      </div>
+
+      <div>
+        <Progress value={50}/>
+        
+      </div>
+
+      <div>
+        <Textarea placeholder={"Textarea"}/>
+        
+        </div>
+        
+        <div>
+        <Checkbox/>
+        
+      </div>
+
+      </div>
     </div>
   );
 }
