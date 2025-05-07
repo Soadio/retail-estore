1436434117e755c684f9f6c5ee3482311fb8ff24
Sheriff Adio <flamezee15@gmail.com>
2025-05-07 16:33:02 +0100

    footer completed

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 
 
 interface Props{
@@ -10,8 +11,12 @@ interface Props{
 const Layout = ({ children}: Props ) => {
     return ( 
         <div className="flex flex-col min-h-screen">
-        <Navbar/>
-        {children}
+            <Navbar />
+            <div className="flex-1 bg-[#F4F4F0]">
+                {children}
+            </div>
+            
+            <Footer />
         </div>
 
      );
